c509f87bbadfe47449244bc812d4432d334cf8fb
Nikola Zrnic <zrnic.nikola@outlook.com>
2023-08-01 13:37:06 +0200

    Response task done

--- index.js
+++ index.js
@@ -20,3 +20,23 @@ app.listen(3000, () => {
 app.get('/users/:userId', (req, res) => {
     res.send(`The user id is: ${req.params.userId}`);
   });
+
+app.get('/math/:op', (req,res) => {
+const x = parseFloat(req.query.x)
+const y = parseFloat(req.query.y)
+
+const {op} = req.params
+
+const result = op === "add" ? x + y : op === "subtract" ? x - y : op === "multiply" ? x + y : op === "divide" ? x / y : false
+
+const resultObject = {
+    numbers : {
+        x: x,
+        y: y
+    },
+    operation: op,
+    result: result ? result : 'Unrecognizable operation name'
+}
+
+res.send(resultObject)
+})
